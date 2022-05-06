449d8d2bf3916578b7e282a10f6570d78a4039e6
yihsien.li <yihsien.li@awea.com>
2022-05-06 13:05:54 +0800

    20220506_Add Serilog

--- Program.cs
+++ Program.cs
@@ -1,25 +1,77 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
-// Add services to the container.
+const string OUTPUT_TEMPLATE = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: OUTPUT_TEMPLATE)
+    .WriteTo.File(@".\Logs\log-.txt", rollingInterval: RollingInterval.Day, outputTemplate: OUTPUT_TEMPLATE)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
+    Log.Information("Starting Web API");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog(Log.Logger, dispose: true);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Jordan Garden API",
+            Description = "An ASP.NET Core Web API for Jordan Garden's business"
+        });
+
+    // using System.Reflection;
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Unite/Error");
+    }
+
+    app.UseSerilogRequestLogging();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Stopped program because of exception");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
